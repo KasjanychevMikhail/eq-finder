cec78ad2db836b6803a66e6c8f4cb09ac8b0fd3f
AlexeyArefev <alexeyarefev1984@mail.ru>
2020-02-22 17:04:16 +0300

    New function for create distance matrix Add  tests for new function + function work

--- test_SystemFun.py
+++ test_SystemFun.py
@@ -55,5 +55,96 @@ class TestInBounds:
     def test_outBoundForAction(self):
         assert sf.inBounds((5, 6), [(1, 1), (0, 6)]) == 0
 
-    def test_wrongforPreCommit(self):
-        assert sf.inBounds((5, 6), [(1, 1), (0, 6)]) == 1
+class TestFindEquilibria:
+    def rhs(self,X,params):
+        x,y=X
+        a,b,c1,c2=params
+        return [x*(y-a*x), y*(x+ b+ y)]
+
+    def rhsJac(self,X, params):
+        x, y = X
+        a, b, c1, c2 = params
+        return np.array([[y - 2 * a * x, x], [y, b + x + 2 * y]])
+
+
+
+    def analyticFind(self,params):
+        a,b,c1,c2=params
+        #nSaddles, nSources, nSinks, nNonRough
+        if ((a>0 and b>0 )  or (a<-1 and b>0 )):
+            result = (1,0,1,1)
+        elif((a>0 and b<0 )or(a<-1 and b<0) ):
+            result = (1,1,0,1)
+        elif(-1<a<0 and b>0):
+            result = (0, 0, 1, 2)
+        elif (-1 < a < 0 and b > 0):
+            result = (0, 1, 0, 2)
+        elif(a == -1 and b>0):
+            result= (0)
+        elif (a == -1 and b < 0):
+            result = (0)
+        elif (a == -1 and b == 0):
+            result = (0,0,0,1)
+        elif (a < -1 and b == 0 or-1< a < 0 and b == 0 or a > 0 and b == 0):
+            result = (0,0,0,1)
+        elif (-1< a < 0 and b == 0):
+            result = (0,0,0,1)
+        return result
+
+    bounds = [(-3.5, 3.5), (-3.5, 3.5)]
+    borders = [(-3.4 - 1e-15, 3.4 + 1e-15), (-3.4 - 1e-15, 3.4 + 1e-15)]
+
+    def test_FindEqInSinglePoint(self):
+        ud = [-0.5,0,0,0]
+        rhsCurrent = lambda X: self.rhs(X, ud)
+        res = sf.findEquilibria(rhsCurrent, self.rhsJac, self.bounds, (1000, 100), ud, self.borders)
+        data= res[:,2:5]
+        print data
+        describe = sf.describePortrType(data.tolist())
+        print describe
+        print self.analyticFind(ud)
+        assert describe == self.analyticFind(ud)
+
+    def test_FindEqInSinglePoint2(self):
+        ud = [-1.5,0.5,0,0]
+        rhsCurrent = lambda X: self.rhs(X, ud)
+        res = sf.findEquilibria(rhsCurrent, self.rhsJac, self.bounds, (300, 30), ud, self.borders)
+        data= res[:,2:5]
+        print data
+        describe = sf.describePortrType(data.tolist())
+        print describe
+        print self.analyticFind(ud)
+        assert describe == self.analyticFind(ud)
+
+    def test_FindEqInSinglePoint3(self):
+        ud = [1.5,0.5,0,0]
+        rhsCurrent = lambda X: self.rhs(X, ud)
+        res = sf.findEquilibria(rhsCurrent, self.rhsJac, self.bounds, (1000, 100), ud, self.borders)
+        data= res[:,2:5]
+        print data
+        describe = sf.describePortrType(data.tolist())
+        print describe
+        print self.analyticFind(ud)
+        assert describe == self.analyticFind(ud)
+
+class TestCreateDistMatrix:
+
+    def testMatrix1(self):
+        coords = [[1, 1], [1, 1]]
+        a = np.array(coords)
+        assert sf.createDistMatrix(a).tolist() == [[0,0],[0,0]]
+
+    def testMatrix2(self):
+        coords = [[0, 0], [2, 2]]
+        a = np.array(coords)
+        assert sf.createDistMatrix(a).tolist() == [[0,2*np.sqrt(2)],[2*np.sqrt(2),0]]
+
+    def testMatrix3(self):
+        coords = [[0, 1], [0, 0]]
+        a = np.array(coords)
+        assert sf.createDistMatrix(a).tolist() == [[0,1],[1,0]]
+
+    def testMatrix4(self):
+        coords = [[2, 2], [2, 2]]
+        a = np.array(coords)
+        assert sf.createDistMatrix(a).tolist() == [[0, 0], [0, 0]]
